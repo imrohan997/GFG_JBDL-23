0d8f9cb830e95c7bd4e55a135dcd67dcca0d81f9
Rohan Agarwal <imrohan997@gmail.com>
2021-12-09 02:28:08 +0530

    Assingment question 10

--- Basic_Core_Java_Assingment/src/com/geeksforgeeks/Main.java
+++ Basic_Core_Java_Assingment/src/com/geeksforgeeks/Main.java
@@ -1,11 +1,14 @@
 package com.geeksforgeeks;
 
+import java.util.Arrays;
+import java.util.Random;
 import java.util.TreeMap;
 
 public class Main {
 
     public static void main(String[] args) {
 
+        //Question 10
         Customer customer1 = new Customer(1, "Ram", "Male", 20);
         Customer customer2 = new Customer(2, "Rashmi", "Female", 40);
         Customer customer3 = new Customer(3, "Kiran", "Female", 30);
@@ -27,6 +30,26 @@ public class Main {
         } else {
             System.out.println(customer + " is present");
         }
+
+        System.out.println("-----------------------------------------------------------");
+
+        //Question 9
+        Integer max = Arrays.asList(23, 21, 11, 1, 45, 90, 56, 34, 22, 33, 44, 2, 6, 3, 55).stream().max((x, y) -> x - y).get();
+        System.out.println(max);
+        Integer min = Arrays.asList(45, 32, 21, 45, 45, 22, 1, 4, 90, 100, 9, 89, 56).stream().min((x, y) -> x - y).get();
+        System.out.println(min);
+
+        System.out.println("-------------------------------------------------------------------");
+
+        //Question 8
+        double average = Arrays.asList(23, 21, 11, 1, 45, 90, 56, 34, 22, 33, 44, 2, 6, 3, 55).stream().mapToInt(x -> x * x).filter(x -> x > 100).average().getAsDouble();
+        System.out.println(average);
+
+        System.out.println("-----------------------------------------------------------------------");
+        //Question 7
+
+        Random random = new Random();
+        random.ints(5).forEach(System.out::println);
 
     }
 }
