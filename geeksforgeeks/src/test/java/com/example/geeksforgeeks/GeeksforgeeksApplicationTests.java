f169a973ded690be6ceaaa490afb81e2090780bd
Rohan Agarwal <imrohan997@gmail.com>
2021-12-12 18:57:11 +0530

    springboot project

---
+++ geeksforgeeks/src/test/java/com/example/geeksforgeeks/GeeksforgeeksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.geeksforgeeks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeeksforgeeksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
