e0b418379c6daee2025008630630e3b657ac8638
Rohan Agarwal <imrohan997@gmail.com>
2023-11-13 23:31:28 +0530

    added Spring profile explanation

--- L5/src/main/java/com/example/Jbdl23DemoForL5Application.java
+++ L5/src/main/java/com/example/Jbdl23DemoForL5Application.java
@@ -73,6 +73,28 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * By default, logs are printed as below
  *
  * 2023-11-05 16:26:02.367  INFO 49172 --- [           main] w.s.c.ServletWebServerApplicationContext : Root WebApplicationContext: initialization completed in 2671 ms
+ *
+ * Spring Profiles-
+ * Spring profiles is a core feature of Springboot which helps to create separation of configuration for
+ * different environments- DEV/QA/PREPROD/PROD
+ *
+ * For eg-For DEV env, we want Logging level as TRACE but for PROD env we want Logging level as INFO,
+ * then we can create to separate application.properties file for each environment as
+ * application-dev.properties
+ * application-qa.properties
+ * application-preprod.properties
+ * application-prod.properties
+ * General syntax will be application-{env name}.properties
+ *
+ * Now, to run application with specific profile there are multiple ways-
+ * From IDE, we can set JVM arg as -Dspring.profiles.active=qa or Environment variable as spring.profiles.active=qa.
+ * This will set the environment which needs to be picked when app starts.
+ *
+ * While running from commandline with specific profile use command java -jar -Dspring.profiles.active={env name} target/L5-0.0.1-SNAPSHOT.jar
+ *
+ * All application-{profileName}.properties is inherited from application.properties(file)[Master property]
+ * When we run application for specific profile all the properties mentioned in application.properties is also taken into consideration along with
+ * application-{env}.properties
  */
 @SpringBootApplication
 public class Jbdl23DemoForL5Application {
